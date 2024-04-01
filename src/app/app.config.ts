c358622b327e52d1a3b497bef1c68a41365ce9dc
Velizar Ganchev <velizar_ganchev@hotmail.com>
2024-04-01 17:19:25 +0200

    create nav, hero and footer components. add poppins font

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,25 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: 'portfolio-e4994',
+          appId: '1:1069471818613:web:ca8e3950ed87b8d03ca93c',
+          storageBucket: 'portfolio-e4994.appspot.com',
+          apiKey: 'AIzaSyDtYR5oui5BNJ4kD4akA8O_5YnOyiXuyWE',
+          authDomain: 'portfolio-e4994.firebaseapp.com',
+          messagingSenderId: '1069471818613',
+        })
+      )
+    ),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+  ],
 };
