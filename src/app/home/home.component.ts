b2c959bd95aafaed93f6e29c4f9397e2393f286e
Velizar Ganchev <velizar_ganchev@hotmail.com>
2024-04-02 20:26:44 +0200

    create and design about component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../components/hero/hero.component';
+import { AboutComponent } from '../components/about/about.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent],
+  imports: [HeroComponent, AboutComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
