219ad4befe7b0fbd4bcefb04ecb792854960e380
Velizar Ganchev <velizar_ganchev@hotmail.com>
2024-04-04 20:52:24 +0200

    create skills component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../components/hero/hero.component';
 import { AboutComponent } from '../components/about/about.component';
+import { SkillsComponent } from '../components/skills/skills.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, AboutComponent],
+  imports: [HeroComponent, AboutComponent, SkillsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
