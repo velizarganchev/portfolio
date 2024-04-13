53becad0e4e3cb2cbc7612187fa36416e0a130b2
Velizar Ganchev <velizar_ganchev@hotmail.com>
2024-04-13 16:51:06 +0200

    create contact component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,12 @@ import { HeroComponent } from '../components/hero/hero.component';
 import { AboutComponent } from '../components/about/about.component';
 import { SkillsComponent } from '../components/skills/skills.component';
 import { PortfolioComponent } from '../components/portfolio/portfolio.component';
+import { ContactComponent } from '../components/contact/contact.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, AboutComponent, SkillsComponent, PortfolioComponent],
+  imports: [HeroComponent, AboutComponent, SkillsComponent, PortfolioComponent, ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
