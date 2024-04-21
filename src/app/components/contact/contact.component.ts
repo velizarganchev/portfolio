b4b1fd38359e0730173ac172899d57294ab07636
Velizar Ganchev <velizar_ganchev@hotmail.com>
2024-04-21 17:18:09 +0200

    add shadows and scroll buttons

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { heroArrowSmallUp } from '@ng-icons/heroicons/outline';
+import { heroUsers } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
+  viewProviders: [provideIcons({ heroUsers, heroArrowSmallUp })],
 })
-export class ContactComponent {
-
-}
+export class ContactComponent {}
--- src/fake-data/fake-data.ts
+++ src/fake-data/fake-data.ts
@@ -43,6 +43,17 @@ const fakeProjects = [
       'https://github.com/velizarganchev/ring-of-fire',
     ],
   },
+  {
+    title: 'Title',
+    technologys: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase'],
+    description:
+      'Task manager inspired by the Kanban System. Create and organize tasks using drag and dropfunctions, assign users and categories.',
+    imgPath: 'assets/img/join-photo.svg',
+    links: [
+      'https://github.com/velizarganchev/koch-welt',
+      'https://github.com/velizarganchev/ring-of-fire',
+    ],
+  },
 ];
 
 export default fakeProjects;
