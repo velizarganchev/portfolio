6363654e02d1c193e72efe5c013f28f5c8b628fe
Velizar Ganchev <velizar_ganchev@hotmail.com>
2024-04-25 21:15:31 +0200

    add mobile navigation

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,13 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import { heroBars3 } from '@ng-icons/heroicons/outline';
+import { heroBars3, heroXMark } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [NgIconComponent],
+  imports: [NgIconComponent, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
-  viewProviders: [provideIcons({ heroBars3 })],
+  viewProviders: [provideIcons({ heroBars3, heroXMark })],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  showNavigation: boolean = true;
+
+  showNav() {
+    console.log(this.showNavigation);
+    
+    this.showNavigation = !this.showNavigation;
+
+
+  }
+}
