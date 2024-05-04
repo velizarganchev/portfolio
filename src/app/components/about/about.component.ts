1383131c1d9ca23b6216171656fe2ede45c9a34b
Velizar Ganchev <velizar_ganchev@hotmail.com>
2024-05-04 19:29:36 +0200

    add some images

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -8,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  @ViewChild('aboutSection') aboutSection!: ElementRef;
+  scrolling: boolean = false;
 
+  ngAfterViewInit(): void {
+    window.addEventListener('scroll', () => {
+      if (!this.scrolling) {
+        this.scrolling = true;
+        setTimeout(() => {
+          this.checkScroll();
+          this.scrolling = false;
+        }, 200); // Verzögerung von 200 Millisekunden
+      }
+    });
+  }
+
+  checkScroll(): void {
+    const element = this.aboutSection.nativeElement;
+    const rect = element.getBoundingClientRect();
+    const elemTop = rect.top;
+    const elemBottom = rect.bottom;
+
+    const isVisible = elemTop >= 0 && elemBottom <= window.innerHeight;
+
+    if (isVisible) {
+      element.classList.add('about-visible');
+    } else {
+      element.classList.remove('about-visible');
+    }
+  }
 }
--- src/fake-data/fake-data.ts
+++ src/fake-data/fake-data.ts
@@ -1,57 +1,57 @@
 const fakeProjects = [
   {
-    title: 'Title',
+    title: 'Join',
+    technologys: ['JavaScript', 'TypeScript', 'HTML', 'CSS', 'Firebase'],
+    description:
+      'Task manager inspired by the Kanban System. Create and organize tasks using drag and dropfunctions, assign users and categories.',
+    imgPath: 'assets/img/portfolio-join.svg',
+    links: [
+      'http://velizar-ganchev.com/join',
+      'https://github.com/velizarganchev/join',
+    ],
+  },
+  {
+    title: 'El Pollo Loco',
+    technologys: ['JavaScript', 'HTML', 'CSS'],
+    description:
+      'A simple jump-and-run game with an object-oriented approach. Assist Pepe in collecting coins and salsa bottles to combat the killer chicken.',
+    imgPath: 'assets/img/portfolio-el-pollo-loco.svg',
+    links: [
+      'http://velizar-ganchev.com/el-pollo-loco',
+      'https://github.com/velizarganchev/DeveloperAkademie/tree/main/html-basics/el-pollo-loco',
+    ],
+  },
+  {
+    title: 'Koch Welt',
     technologys: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase'],
     description:
       'Task manager inspired by the Kanban System. Create and organize tasks using drag and dropfunctions, assign users and categories.',
     imgPath: 'assets/img/join-photo.svg',
     links: [
+      'http://velizar-ganchev.com/koch-welt',
       'https://github.com/velizarganchev/koch-welt',
+    ],
+  },
+  {
+    title: 'Mon Ami Patisseri',
+    technologys: ['React', 'JavaScript', 'HTML', 'CSS', 'mongoDB'],
+    description:
+      'Task manager inspired by the Kanban System. Create and organize tasks using drag and dropfunctions, assign users and categories.',
+    imgPath: '../assets/img/portfolio-monAmiPatisseri.svg',
+    links: [
+      'http://velizar-ganchev.com/ring-of-fire',
       'https://github.com/velizarganchev/ring-of-fire',
     ],
   },
   {
-    title: 'Title',
-    technologys: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase'],
+    title: 'Real Estate',
+    technologys: ['React', 'JavaScript', 'HTML', 'CSS', 'mongoDB'],
     description:
       'Task manager inspired by the Kanban System. Create and organize tasks using drag and dropfunctions, assign users and categories.',
-    imgPath: 'assets/img/join-photo.svg',
+    imgPath: 'assets/img/portfolio-real-estate.svg',
     links: [
-      'https://github.com/velizarganchev/koch-welt',
-      'https://github.com/velizarganchev/ring-of-fire',
-    ],
-  },
-  {
-    title: 'Title',
-    technologys: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase'],
-    description:
-      'Task manager inspired by the Kanban System. Create and organize tasks using drag and dropfunctions, assign users and categories.',
-    imgPath: 'assets/img/join-photo.svg',
-    links: [
-      'https://github.com/velizarganchev/koch-welt',
-      'https://github.com/velizarganchev/ring-of-fire',
-    ],
-  },
-  {
-    title: 'Title',
-    technologys: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase'],
-    description:
-      'Task manager inspired by the Kanban System. Create and organize tasks using drag and dropfunctions, assign users and categories.',
-    imgPath: 'assets/img/join-photo.svg',
-    links: [
-      'https://github.com/velizarganchev/koch-welt',
-      'https://github.com/velizarganchev/ring-of-fire',
-    ],
-  },
-  {
-    title: 'Title',
-    technologys: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase'],
-    description:
-      'Task manager inspired by the Kanban System. Create and organize tasks using drag and dropfunctions, assign users and categories.',
-    imgPath: 'assets/img/join-photo.svg',
-    links: [
-      'https://github.com/velizarganchev/koch-welt',
-      'https://github.com/velizarganchev/ring-of-fire',
+      'https://real-estatev.vercel.app/',
+      'https://github.com/velizarganchev/real-estate',
     ],
   },
 ];
